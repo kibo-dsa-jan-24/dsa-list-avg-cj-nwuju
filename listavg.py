4c1169f9d0ff49a46fd1846a53dd7ab85df9c3a4
CJ Nwuju <chukwudi.nwuju@kibo.school>
2024-02-14 20:18:51 +0100

    done

--- listavg.py
+++ listavg.py
@@ -1,9 +1,13 @@
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
+        self.total = 0
+        for num in self.lst:
+            self.total += num
 
     def add(self, num):
         self.lst.append(num)
+        self.total += num
 
     def compute_avg(self):
         total = 0
@@ -12,5 +16,4 @@ class ListAverage:
         return total / len(self.lst)
 
     def compute_avg_faster(self):
-        # implement this method
-        pass
+        return self.total/ len(self.lst)
--- test_listavg.py
+++ test_listavg.py
@@ -7,3 +7,11 @@ class TestListAverage(unittest.TestCase):
         assert lavg.compute_avg() == 2.0
 
     # add more unit tests below
+    def test_faster(self):
+        lavg = ListAverage([2,4,6])
+        self.assertTrue(lavg.compute_avg_faster(), 4.0)
+
+    def test_faster_with_add(self):
+        lavg = ListAverage([2,4,6])
+        lavg.add(8)
+        self.assertTrue(lavg.compute_avg_faster(), 5.0)
